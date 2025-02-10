e4f8306d0a7735238b2a29b7d71dec9e2249642c
Killreel <killreally@gmail.com>
2025-02-10 18:54:43 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,11 @@
 -- write your code for database user creation here
+CREATE USER 'webappuser' @'%' IDENTIFIED BY 'P@ssw0rd';
+
+-- Надаємо користувачеві webappuser права на виконання CRUD-операцій
+GRANT SELECT, INSERT , UPDATE, DELETE ON ShopDB.* TO 'webappuser' @'%';
+
+-- Створюємо користувача для деплою
+CREATE USER 'deploymentuser' @'%' IDENTIFIED BY 'P@ssw0rd';
+
+-- Надаємо користувачеві deploymentuser повні права на базу даних
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser' @'%';
