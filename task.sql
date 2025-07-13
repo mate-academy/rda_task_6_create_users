1c2628ed2a98b4c81282f37528a2be5d161755c7
Denys Sheremeta <DSheremeta@franpos.com>
2025-07-13 17:13:20 +0300

    Solution1

--- task.sql
+++ task.sql
@@ -3,4 +3,4 @@ CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL ON ShopDB.* TO 'webappuser'@'%';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
