1d8c87f585cdfa27c2764be82ba6ec5da3dfdfd3
Yurii Keniu <kenu21@gmail.com>
2025-09-20 18:13:35 +0200

    grant-privileges: formatted

--- task.sql
+++ task.sql
@@ -1,5 +1,4 @@
-​​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-​​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
 GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
