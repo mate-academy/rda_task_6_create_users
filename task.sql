38792725af75394d6b28e047803e37e8bd5dfdc8
Ustiantsev Vladislav <vladisloveust@gmail.com>
2025-09-22 11:04:58 +0300

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,16 @@
--- write your code for database user creation here
+USE ShopDB;
+
+-- створення користувача для вебзастосунку
+CREATE USER IF NOT EXISTS 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+-- CRUD-права (тільки базові операції)
+GRANT SELECT, INSERT, UPDATE, DELETE ON `ShopDB`.* TO 'webappuser'@'%';
+
+-- створення користувача для deployment
+CREATE USER IF NOT EXISTS 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+-- Повні права на базу
+GRANT ALL PRIVILEGES ON `ShopDB`.* TO 'deploymentuser'@'%';
+
+-- оновити кеш привілеїв
+FLUSH PRIVILEGES;
