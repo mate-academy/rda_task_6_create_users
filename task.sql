27a84b74a36b355c6c4cf3cbbb95535fc3b246cd
unlie9 <unlie.devops9@gmail.com>
2026-01-18 23:30:17 +0200

    sol

--- task.sql
+++ task.sql
@@ -1,1 +1,12 @@
 -- write your code for database user creation here
+
+-- Create webappuser with CRUD permissions (SELECT, INSERT, UPDATE, DELETE)
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create deploymentuser with all permissions on ShopDB
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+-- Apply changes
+FLUSH PRIVILEGES;
