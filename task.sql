95d8fbbebec6dee285249b0cf840f254c082d8dd
Danylo Moroz <danylomoroz1989@gmail.com>
2026-01-25 14:08:22 +0100

    removed_extra_space_from_grant

--- task.sql
+++ task.sql
@@ -3,4 +3,4 @@ CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL ON ShopDB.* TO 'deploymentuser '@'%';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
