2024ba16fd1cf27a7f568ac0821b134f75a35157
Danylo Povar <danila74povar@gmail.com>
2026-01-28 19:51:51 +0200

    task_6

--- task.sql
+++ task.sql
@@ -1,1 +1,15 @@
 -- write your code for database user creation here
+
+DROP USER IF EXISTS 'webappuser'@'%';
+
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+DROP USER IF EXISTS 'deploymentuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
