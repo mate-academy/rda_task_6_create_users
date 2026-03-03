a841d80f62a805f97ec278df886db3a9dd3df59f
Lumos <tsykalanovdima@gmail.com>
2026-03-03 20:50:38 +0000

    Solution

--- task.sql
+++ task.sql
@@ -1,1 +1,9 @@
--- write your code for database user creation here
+-- Web application user: basic CRUD on ShopDB
+CREATE USER IF NOT EXISTS 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Deployment pipeline user: full access to ShopDB
+CREATE USER IF NOT EXISTS 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
