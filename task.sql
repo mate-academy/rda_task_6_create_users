28c00290b5daabacb5b9437ffb82cc10f40ab241
Yuriy Mishchuk <yuramishchuk1703@gmail.com>
2026-04-24 14:47:04 +0300

    Create user

--- task.sql
+++ task.sql
@@ -1,6 +1,7 @@
 -- write your code for database user creation here
 
-​​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%'; 
