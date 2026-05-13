d3396cd39b432ce3c46f3304d9065f4e2b872253
maksy <maksym.petliuk@gmail.com>
2026-05-13 14:09:18 +0300

    Add database users and configure permissions

--- task.sql
+++ task.sql
@@ -1,1 +1,15 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'localhost'
+IDENTIFIED BY 'P@ssw0rd';
+
+GRANT SELECT, INSERT, UPDATE, DELETE
+ON ShopDB.*
+TO 'webappuser'@'localhost';
+
+CREATE USER 'deploymentuser'@'localhost'
+IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL PRIVILEGES
+ON ShopDB.*
+TO 'deploymentuser'@'localhost';
+
+FLUSH PRIVILEGES;
