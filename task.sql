6dd10d4ac6298df38aef699ec57674b2a309073d
Ivan Sotsenko <vanja.sotsenko@gmail.com>
2026-05-16 22:25:29 +0200

    Create two users with diferent privileges

--- task.sql
+++ task.sql
@@ -1,1 +1,4 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT,DELETE,UPDATE,INSERT ON ShopDB.* TO 'webappuser'@'%';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
