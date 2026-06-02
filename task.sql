4b9371747146969669712ac75c83f5c7d4286ae8
Tetiana Savenko <tanyasavenko5@gmail.com>
2026-06-02 15:20:57 +0300

    'Solution2'

--- task.sql
+++ task.sql
@@ -2,6 +2,6 @@ CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
 
 FLUSH PRIVILEGES;
