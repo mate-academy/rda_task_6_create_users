74a6a80b0255e18fee0ae72a57ce249aca8e295e
Foxytail8 <xinenax@gmail.com>
2026-06-09 10:22:52 +0300

    'Solution2'

--- task.sql
+++ task.sql
@@ -1,5 +1,8 @@
--- write your code for database user creation here
+
 ​​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
 ​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
 GRANT INSERT, UPDATE, DELETE, SELECT ON ShopDB.* TO 'webappuser'@'%'; 
+
 GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
