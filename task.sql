ed92cd3bb5ab3061f641fa651bff3172d42a6ab8
Artem <artemkluenkov41@gmail.com>
2026-06-17 18:31:30 +0000

    solution

--- task.sql
+++ task.sql
@@ -1,12 +1,4 @@
--- 1. Create the webappuser and grant basic CRUD permissions
-CREATE USER 'webappuser'@'localhost' IDENTIFIED BY 'P@ssw0rd';
-
-GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'localhost';
-
--- 2. Create the deploymentuser and grant full administrative privileges
-CREATE USER 'deploymentuser'@'localhost' IDENTIFIED BY 'P@ssw0rd';
-
-GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'localhost';
-
--- 3. Apply the changes immediately
-FLUSH PRIVILEGES;
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
