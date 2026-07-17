c9dec3e41c9701e746489be452e6a3e51c83e03b
Illia Fedoruk <fedorukstar@gmail.com>
2026-07-17 15:17:11 +0300

    Create webappuser and deploymentuser with specific permissions

--- task.sql
+++ task.sql
@@ -1,1 +1,10 @@
 -- write your code for database user creation here
+DROP DATABASE IF EXISTS ShopDB;
+CREATE DATABASE ShopDB;
+USE ShopDB;
+
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT INSERT, SELECT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL ON ShopDB.* TO 'deploymentuser'@'%';
