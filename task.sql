524ef1fb1e518656d63b2c38987a33b4b417a027
Your Nameclr <you@example.com>
2026-07-20 17:51:36 +0200

    Create webappuser and deploymentuser with appropriate ShopDB permissions

--- task.sql
+++ task.sql
@@ -1,1 +1,9 @@
--- write your code for database user creation here
+-- Create webappuser with basic CRUD permissions
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+-- Create deploymentuser with full permissions on ShopDB
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;   
