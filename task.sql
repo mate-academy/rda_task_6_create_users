3ccfaa69a1ca4830aa71bcbcd8be8dcdf0a54e8e
Vadym23-11 <vadym.rurka01@gmail.com>
2026-07-28 21:17:55 +0200

    New solution

--- task.sql
+++ task.sql
@@ -1,9 +1,8 @@
-create database ShopDB;
-use ShopDB;
-
-Create user 'webappuser'@'%' identified BY 'P@ssw0rd'; 
+Create user 'webappuser'@'%' identified BY 'P@ssw0rd';
 Create user 'deploymentuser'@'%' identified BY 'P@ssw0rd';
 
 GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
-GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%'; 
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
+
+FLUSH PRIVILEGES;
