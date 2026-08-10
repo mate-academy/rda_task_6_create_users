2ee93e41b201a4c885fa75f38e51a2b63b4d1412
godfri228 <overe14@gmail.com>
2026-08-10 19:07:21 +0300

    Fix user host to % for CI tests

--- task.sql
+++ task.sql
@@ -1,9 +1,9 @@
 -- Create webappuser: basic CRUD only (no DDL / table creation)
-CREATE USER 'webappuser'@'localhost' IDENTIFIED BY 'P@ssw0rd';
-GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'localhost';
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 -- Create deploymentuser: full access, including creating new tables
-CREATE USER 'deploymentuser'@'localhost' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'localhost';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
 
 FLUSH PRIVILEGES;
