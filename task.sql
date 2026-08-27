674de7069cf1ba56fb12c1a5e18d4f07fd70e33b
Tetiana Mohorian <tetiana.mohorian@student.tuke.sk>
2026-08-27 17:17:03 +0200

    Solution

--- task.sql
+++ task.sql
@@ -1,4 +1,4 @@
-​​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, SELECT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 ​​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
