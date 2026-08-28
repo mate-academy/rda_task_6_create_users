8f21453d2e4ee5626cb007aa124a05d3245aa757
Tetiana Mohorian <tetiana.mohorian@student.tuke.sk>
2026-08-28 09:30:04 +0200

    Update task.sql

--- task.sql
+++ task.sql
@@ -1,5 +1,5 @@
-​CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT INSERT, SELECT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
-​​CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
