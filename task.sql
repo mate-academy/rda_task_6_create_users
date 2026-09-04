2940c0350f1be9b351b1bf322599605e8b9ace43
Mykola Hakh <kolyagakh@gmail.com>
2026-09-04 12:15:33 +0300

    Lesson complete

--- task.sql
+++ task.sql
@@ -2,18 +2,14 @@
 
 USE ShopDB;
 
-CREATE USER 'webappuser'@'%' IDENTIFIED by 'P@ssw0rd'
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
-
 ON ShopDB.*
-
 TO 'webappuser'@'%';
 
-CREATE USER 'deploymentuser'@'%' IDENTIFIED by 'P@ssw0rd'
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
 
 GRANT ALL PRIVILEGES
-
 ON ShopDB.*
-
 TO 'deploymentuser'@'%';
