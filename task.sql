96e0b81d98ccfa9d9859eb748649928225e8e054
Dmytro <@email.com>
2026-09-06 21:50:59 +0300

    lol

--- task.sql
+++ task.sql
@@ -1,7 +1,7 @@
-CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@sswOrd';
-GRANT CREATE, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
 
 CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
-GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%' ;
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%';
 
 FLUSH PRIVILEGES;
