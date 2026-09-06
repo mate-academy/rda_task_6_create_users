5e44114ae0a2ef15b4578ab707f13fd328f9a956
Dmytro <@email.com>
2026-09-06 21:47:08 +0300

    lol

--- task.sql
+++ task.sql
@@ -1,1 +1,7 @@
--- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@sswOrd';
+GRANT CREATE, INSERT, UPDATE, DELETE ON ShopDB.* TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+GRANT ALL PRIVILEGES ON ShopDB.* TO 'deploymentuser'@'%' ;
+
+FLUSH PRIVILEGES;
