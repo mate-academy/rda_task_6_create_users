4b76634c114415bd779d0b1a65041192d5091bf8
Daniil <shpak.daniil06@gmail.com>
2026-09-07 18:37:38 +0300

    Create database users

--- task.sql
+++ task.sql
@@ -1,1 +1,10 @@
 -- write your code for database user creation here
+CREATE USER 'webappuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT SELECT, INSERT, UPDATE, DELETE ON ShopDB.*
+TO 'webappuser'@'%';
+
+CREATE USER 'deploymentuser'@'%' IDENTIFIED BY 'P@ssw0rd';
+
+GRANT ALL ON ShopDB.*
+TO 'deploymentuser'@'%';
